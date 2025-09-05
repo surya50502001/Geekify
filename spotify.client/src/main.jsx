53ec376bce3852c8ec392797951d84b96b46814b
surya <surya@PRIME06>
2025-09-05 15:40:18 +0530

    ch

--- spotify.client/src/main.jsx
+++ spotify.client/src/main.jsx
@@ -3,14 +3,14 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
-// Register service worker for PWA
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/sw.js')
-      .then(registration => console.log('SW registered'))
-      .catch(error => console.log('SW registration failed'));
-  });
-}
+// Service worker disabled for easier development
+// if ('serviceWorker' in navigator) {
+//   window.addEventListener('load', () => {
+//     navigator.serviceWorker.register('/sw.js')
+//       .then(registration => console.log('SW registered'))
+//       .catch(error => console.log('SW registration failed'));
+//   });
+// }
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>

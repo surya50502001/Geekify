80ac9464bdb068cd29b53ac669af6d930826942d
surya <surya@PRIME06>
2025-09-09 12:39:02 +0530

    ch

--- spotify.client/src/App.jsx
+++ spotify.client/src/App.jsx
@@ -11,10 +11,6 @@ function App() {
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    const viewport = document.querySelector('meta[name=viewport]');
-    if (viewport) {
-      viewport.setAttribute('content', 'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no');
-    }
     document.addEventListener('touchstart', () => {}, { passive: true });
     
     // Fetch songs from GitHub
